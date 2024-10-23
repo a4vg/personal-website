dc3cc7698fbaf2bd553921c43710dbf9a9a871d3
Andrea Velasquez <andreavelasquez3107@gmail.com>
2024-10-23 15:08:32 +0100

    refactor: move data to a separate folder

---
+++ src/data/socialLinks.ts
@@ -0,0 +1,24 @@
+import LinkedinIcon from "../assets/images/icons/linkedin.svg";
+import GithubIcon from "../assets/images/icons/github.svg";
+import EmailIcon from "../assets/images/icons/email.svg";
+
+export default [
+  {
+    name: "linkedin",
+    icon: LinkedinIcon,
+    label: "linkedin.com/in/a4vg",
+    link: "https://linkedin.com/in/a4vg",
+  },
+  {
+    name: "github",
+    icon: GithubIcon,
+    label: "@a4vg",
+    link: "https://github.com/a4vg",
+  },
+  {
+    name: "email",
+    icon: EmailIcon,
+    label: "andreav@duck.com",
+    link: "mailto:andreav@duck.com",
+  },
+];
