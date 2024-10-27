cff239a81103b8a572e8218ce5754d3523761e04
Andrea Velasquez <andreavelasquez3107@gmail.com>
2024-10-27 23:54:19 +0100

    fix: xs overriding larger breakpoints

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,21 @@
+import { screens } from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    screens: {
+      // smaller breakpoints should be added before the rest
+      // placing them in "extend" would put them after
+      xs: "540px",
+      ...screens,
+    },
     extend: {
       fontFamily: {
         sans: ["Roboto Flex", "sans-serif"],
       },
       colors: {
         "main-red": "#B31221",
-      },
-      screens: {
-        xs: "540px",
       },
     },
   },
