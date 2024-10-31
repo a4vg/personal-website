81126ac233c8d0d192508f68fa53997063cab947
Andrea Velasquez <andreavelasquez3107@gmail.com>
2024-10-31 15:25:21 +0100

    feat: add data for skills

---
+++ src/data/skills.ts
@@ -0,0 +1,196 @@
+import TypescriptIcon from "../assets/images/icons/typescript.png";
+import NodeJsIcon from "../assets/images/icons/nodejs.png";
+import JavaIcon from "../assets/images/icons/java.png";
+import PythonIcon from "../assets/images/icons/python.png";
+import GoIcon from "../assets/images/icons/go.png";
+import CPlusPlusIcon from "../assets/images/icons/cplusplus.png";
+import SQLIcon from "../assets/images/icons/sql.png";
+import HTMLIcon from "../assets/images/icons/html.png";
+import CSSIcon from "../assets/images/icons/css.png";
+import PostgreSQLIcon from "../assets/images/icons/postgresql.png";
+import OracleIcon from "../assets/images/icons/oracle.png";
+import SQLiteIcon from "../assets/images/icons/sqlite.png";
+import DynamoDBIcon from "../assets/images/icons/dynamodb.png";
+import MongoDBIcon from "../assets/images/icons/mongodb.png";
+import SnowflakeIcon from "../assets/images/icons/snowflake.png";
+import DuckDBIcon from "../assets/images/icons/duckdb.png";
+import FlaskIcon from "../assets/images/icons/flask.png";
+import KyselyIcon from "../assets/images/icons/kysely.png";
+import ZodIcon from "../assets/images/icons/zod.png";
+import SvelteIcon from "../assets/images/icons/svelte.png";
+import NextJsIcon from "../assets/images/icons/nextjs.png";
+import ReactIcon from "../assets/images/icons/react.png";
+import ReduxIcon from "../assets/images/icons/redux.png";
+import TailwindIcon from "../assets/images/icons/tailwind.png";
+import JestIcon from "../assets/images/icons/jest.png";
+import MochaIcon from "../assets/images/icons/mocha.png";
+import AWSIcon from "../assets/images/icons/aws.png";
+import GoogleCloudIcon from "../assets/images/icons/google_cloud.png";
+import BitbucketIcon from "../assets/images/icons/bitbucket.png";
+import GithubIcon from "../assets/images/icons/github.png";
+import AWSCodePipelineIcon from "../assets/images/icons/aws_codepipeline.png";
+import DockerIcon from "../assets/images/icons/docker.png";
+import GitIcon from "../assets/images/icons/git.png";
+import TerminalIcon from "../assets/images/icons/terminal.png";
+import GoogleAnalyticsIcon from "../assets/images/icons/google_analytics.png";
+
+export default {
+  languages: [
+    {
+      name: "Typescript",
+      icon: TypescriptIcon,
+    },
+    {
+      name: "Node.js",
+      icon: NodeJsIcon,
+    },
+    {
+      name: "Java",
+      icon: JavaIcon,
+    },
+    {
+      name: "Python",
+      icon: PythonIcon,
+    },
+    {
+      name: "Go",
+      icon: GoIcon,
+    },
+    {
+      name: "C++",
+      icon: CPlusPlusIcon,
+    },
+    {
+      name: "SQL",
+      icon: SQLIcon,
+    },
+    {
+      name: "HTML",
+      icon: HTMLIcon,
+    },
+    {
+      name: "CSS",
+      icon: CSSIcon,
+    },
+  ],
+  databases: {
+    relational: [
+      {
+        name: "PostgreSQL",
+        icon: PostgreSQLIcon,
+      },
+      {
+        name: "Oracle",
+        icon: OracleIcon,
+      },
+      {
+        name: "SQLite",
+        icon: SQLiteIcon,
+      },
+    ],
+    nonRelational: [
+      {
+        name: "DynamoDB",
+        icon: DynamoDBIcon,
+      },
+      {
+        name: "MongoDB",
+        icon: MongoDBIcon,
+      },
+    ],
+    warehouse: [
+      {
+        name: "Snowflake",
+        icon: SnowflakeIcon,
+      },
+      {
+        name: "DuckDB",
+        icon: DuckDBIcon,
+      },
+    ],
+  },
+  librariesAndFrameworks: [
+    {
+      name: "Flask",
+      icon: FlaskIcon,
+    },
+    {
+      name: "Kysely",
+      icon: KyselyIcon,
+    },
+    {
+      name: "Zod",
+      icon: ZodIcon,
+    },
+    {
+      name: "SvelteKit",
+      icon: SvelteIcon,
+    },
+    {
+      name: "Next.js",
+      icon: NextJsIcon,
+    },
+    {
+      name: "React",
+      icon: ReactIcon,
+    },
+    {
+      name: "Redux",
+      icon: ReduxIcon,
+    },
+    {
+      name: "Tailwind",
+      icon: TailwindIcon,
+    },
+    {
+      name: "Jest",
+      icon: JestIcon,
+    },
+    {
+      name: "Mocha",
+      icon: MochaIcon,
+    },
+  ],
+  cloud: [
+    {
+      name: "AWS",
+      icon: AWSIcon,
+    },
+    {
+      name: "Google Cloud",
+      icon: GoogleCloudIcon,
+    },
+  ],
+  cicd: [
+    {
+      name: "Bitbucket Pipelines",
+      icon: BitbucketIcon,
+    },
+    {
+      name: "Github Actions",
+      icon: GithubIcon,
+    },
+    {
+      name: "AWS CodePipeline",
+      icon: AWSCodePipelineIcon,
+    },
+  ],
+  others: [
+    {
+      name: "Docker",
+      icon: DockerIcon,
+    },
+    {
+      name: "Git",
+      icon: GitIcon,
+    },
+    {
+      name: "Terminal",
+      icon: TerminalIcon,
+    },
+    {
+      name: "Google Analytics",
+      icon: GoogleAnalyticsIcon,
+    },
+  ],
+};
